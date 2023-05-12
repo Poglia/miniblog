8cbf3d25b122785ea805218ef66ed5594dec8319
Poglia <pedro.poglia@hotmail.com>
2023-05-12 16:00:48 -0300

    feat: install r-router-dom and config on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// PAGES
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   return (
-    <div className="container">
-        <h1>Miniblog</h1>
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
